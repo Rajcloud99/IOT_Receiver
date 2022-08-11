ac4a3685d6cb049974722164c6e45b2fee43bfbd
kamal <kamal.mewada.nitt@gmail.com>
2022-08-11 07:55:51 +0530

    more config in defaults

--- config/default.js
+++ config/default.js
@@ -510,5 +510,13 @@ module.exports = {
             "time" : '00 00 03 * * *'
         }
     },
+    "lms":{
+        lmsService:true,
+        host:"3.110.92.242",
+        port:"3002",
+        userAllowedForLiveFeedForAll:true,
+        userAllowedForTripForAll:true,
+        server_intercommunication_key:"eyJrZXkiOiJncHNnYWFkaSIsInRpbWUiOjE1MjIwMTA0OTgxNjJ9.0kD2fPtK2cybSL1rU51_azToU_SEr_TcNXPOzKgUxyA"
+    },
     "notification":{}
 };
